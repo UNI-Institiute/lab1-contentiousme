9f9af2f144a546a8cff9f91a9a61e29a285f2951
repl.it user <replituser@example.com>
2020-09-09 01:23:11 +0000

    hello

--- vehicle/vehicle_tests.py
+++ vehicle/vehicle_tests.py
@@ -2,9 +2,9 @@ import unittest
 import vehicle
 
 class VehicleTests(unittest.TestCase):
-    def test_vehicle(self):
-    # Add code here.
-
+  def test_vehicle(self):
+    testVehicle = vehicle.Vehicle(4,40,2,True)
+    
 
 # Run the unit tests.
 if __name__ == '__main__':
